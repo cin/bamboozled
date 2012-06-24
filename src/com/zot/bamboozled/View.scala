3315a3dcff5d0bd138194fcf0199cf56854b8ba7
cin <cingram311@hotmail.com>
2012-06-23 23:40:22 -0400

    touch events and renderer added

---
+++ src/com/zot/bamboozled/View.scala
@@ -0,0 +1,13 @@
+package com.zot.bamboozled
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cin
+ * Date: 6/23/12
+ * Time: 11:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class View {
+
+}
